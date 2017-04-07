dd27863df884b2914c6ab7517297b4141c00e55b
dr6kl <dr6kl@tutanota.com>
2017-04-07 22:30:53 +0200

    Proceed with lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,13 +8,15 @@ contract Lottery {
 
   struct Round {
     Participant[] participants;
+    uint pot;
     address winner;
+    bool prizeIsPaid;
   }
 
   uint public startBlock;
   uint public duration;
 
-  mapping (uint => Round) rounds;
+  mapping (uint => Round) public rounds;
 
   function Lottery(uint _duration) {
     startBlock = block.number;
@@ -26,6 +28,7 @@ contract Lottery {
     uint i = rounds[r].participants.length++;
     rounds[r].participants[i].addr = msg.sender;
     rounds[r].participants[i].amount = msg.value;
+    rounds[r].pot += msg.value;
   }
 
   function currentRound() constant returns (uint) {
@@ -34,5 +37,37 @@ contract Lottery {
 
   function getParticipantsLength(uint num) constant returns (uint) {
     return rounds[num].participants.length;
+  }
+
+  function determineWinner(uint roundNum) {
+    if (!(roundNum < currentRound())) {
+      throw;
+    }
+    Round round = rounds[roundNum];
+    if (round.winner != 0) {
+      throw;
+    }
+    Participant[] participants = round.participants;
+    uint rand = uint(block.blockhash(startBlock + (roundNum + 1) * duration)) % round.pot;
+
+    uint s = 0;
+    for (uint i = 0; i < participants.length; i++) {
+      s += participants[i].amount;
+      if (s > rand) {
+        round.winner = participants[i].addr;
+        break;
+      }
+    }
+  }
+
+  function getPrize(uint roundNum) {
+    Round round = rounds[roundNum];
+    if (!(round.winner == msg.sender) || round.prizeIsPaid) {
+      throw;
+    }
+    round.prizeIsPaid = true;
+    if (!round.winner.send(round.pot)) {
+      round.prizeIsPaid = false;
+    }
   }
 }
--- test/lottery.js
+++ test/lottery.js
@@ -14,27 +14,58 @@ var skipBlocks = function(n) {
   });
 }
 
+var addParticipants = function(lottery, n) {
+  var addParticipant = function(i) {
+    if (i == n) {
+      return Promise.resolve();
+    } else {
+      return lottery.participate({value: web3.toWei(2), from: web3.eth.accounts[i]}).then(function() {return addParticipant(i + 1)});
+    }
+  }
+  return addParticipant(0);
+}
+
 contract('Lottery', function(accounts) {
   it('works', function() {
     return Lottery.new(10).then(function(lottery) {
-      return lottery.participate({value: web3.toWei(1), from: accounts[0]}).then(function() {
-        return lottery.participate({value: web3.toWei(2), from: accounts[1]});
+      return addParticipants(lottery, 3).then(function() {
+        return lottery.determineWinner(0); // fails as round is not finished
       }).then(function() {
-        return lottery.participate({value: web3.toWei(3), from: accounts[2]});
+        return lottery.rounds(0);
+      }).then(function(r) {
+        assert.equal(r[0], web3.toWei(6));
+        assert.equal(r[1], 0);
+        assert.equal(r[2], false);
       }).then(function() {
         return skipBlocks(10);
       }).then(function() {
-        return lottery.participate({value: web3.toWei(1), from: accounts[0]});
-      }).then(function() {
-        return lottery.participate({value: web3.toWei(1), from: accounts[1]});
+        return addParticipants(lottery, 2);
       }).then(function() {
         return lottery.getParticipantsLength(0);
       }).then(function(l) {
         return assert.equal(l, 3);
       }).then(function() {
-        return lottery.getParticipantsLength(1);
-      }).then(function(l) {
-        return assert.equal(l, 2);
+        return lottery.determineWinner(0);
+      }).then(function() {
+        return lottery.rounds(0);
+      }).then(function(r) {
+        assert.equal(r[0], web3.toWei(6));
+        assert.include(web3.eth.accounts, r[1]);
+        assert.equal(r[2], false);
+        return;
+      }).then(function() {
+        return lottery.getPrize(0, {from: web3.eth.accounts[3]});
+      }).then(function() {
+        return lottery.rounds(0);
+      }).then(function(r) {
+        assert.equal(r[2], false);
+        return r[1];
+      }).then(function(winner) {
+        return lottery.getPrize(0, {from: winner});
+      }).then(function() {
+        return lottery.rounds(0);
+      }).then(function(r) {
+        assert.equal(r[2], true);
       });
     });
   });
